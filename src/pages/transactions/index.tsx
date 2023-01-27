2c13378df281e304495b2941a1388675872ea14f
*Gustavo <guga77sugu@hotmail.com>
2023-01-27 11:44:50 -0300

    test

---
+++ src/pages/transactions/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from '../../components/Header/Header'
+import styles from './index.module.css'
+
+export function Transactions() {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
