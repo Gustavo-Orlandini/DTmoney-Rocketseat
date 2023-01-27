dddeabdda355241e3159661da70416449f2c7815
*Gustavo <guga77sugu@hotmail.com>
2023-01-27 13:59:14 -0300

    Cards finished

--- src/components/Header/Summary/index.tsx
+++ src/components/Header/Summary/index.tsx
@@ -1,11 +1,39 @@
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
 import styles from './summary.module.css'
+
 
 export function Summary() {
     return (
         <div className={styles.SummaryContainer}>
-            <header>
-                <span>Entradas</span>
-            </header>
+            <div className={styles.SummaryCard}>
+
+                <header>
+                    <span>Entradas</span>
+                    <ArrowCircleUp size={32} color='#00b37e' />
+                </header>
+
+                <strong>R$ 17.400,00</strong>
+            </div>
+
+            <div className={styles.SummaryCard}>
+
+                <header>
+                    <span>Saídas</span>
+                    <ArrowCircleDown size={32} color='#f75a68' />
+                </header>
+
+                <strong>R$ 17.400,00</strong>
+            </div>
+
+            <div className={styles.SummaryCard}>
+
+                <header>
+                    <span>Total</span>
+                    <CurrencyDollar size={32} color='#fff' />
+                </header>
+
+                <strong>R$ 17.400,00</strong>
+            </div>
 
         </div>
     )
