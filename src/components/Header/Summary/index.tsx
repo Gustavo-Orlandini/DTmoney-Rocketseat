20c7ca454dec08e0f94010e65d3127f30b668e1f
*Gustavo <guga77sugu@hotmail.com>
2023-01-28 18:44:41 -0300

    Function in Sumary ".reduce( )" and show results

--- src/components/Header/Summary/index.tsx
+++ src/components/Header/Summary/index.tsx
@@ -1,8 +1,26 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
+import { TransactionsContext } from '../../../Contexts/TransactionsContext'
+import { useContext } from 'react';
 import styles from './summary.module.css'
 
 
 export function Summary() {
+    const { transactions } = useContext(TransactionsContext);
+
+    const sumary = transactions.reduce(
+        (acc, transaction) => {
+
+            if (transaction.type === 'income') {
+                acc.income += transaction.price;
+                acc.total += transaction.price;
+            } else {
+                acc.outcome += transaction.price;
+                acc.total -= transaction.price;
+            }
+
+            return acc;
+        }, { income: 0, outcome: 0, total: 0 })
+
     return (
         <div className={styles.SummaryContainer}>
             <div className={styles.SummaryCard}>
@@ -12,7 +30,7 @@ export function Summary() {
                     <ArrowCircleUp size={32} color='#00b37e' />
                 </header>
 
-                <strong>R$ 17.400,00</strong>
+                <strong>{sumary.income}</strong>
             </div>
 
             <div className={styles.SummaryCard}>
@@ -22,7 +40,7 @@ export function Summary() {
                     <ArrowCircleDown size={32} color='#f75a68' />
                 </header>
 
-                <strong>R$ 17.400,00</strong>
+                <strong>{sumary.outcome}</strong>
             </div>
 
             <div className={styles.SummaryCard}>
@@ -32,9 +50,11 @@ export function Summary() {
                     <CurrencyDollar size={32} color='#fff' />
                 </header>
 
-                <strong>R$ 17.400,00</strong>
+                <strong>{sumary.total}</strong>
             </div>
 
         </div>
     )
 }
+
+
