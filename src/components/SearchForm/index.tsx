492dd5ac9414fef1bc3de38bd50a50bd194bbbca
*Gustavo <guga77sugu@hotmail.com>
2023-01-30 10:44:18 -0300

    Search form funcionality ok! install axios

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -3,6 +3,8 @@ import { useForm } from 'react-hook-form'
 import styles from './searchForm.module.css'
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useContext } from 'react'
+import { TransactionsContext } from '../../Contexts/TransactionsContext'
 
 const searchFormSchema = z.object({
     query: z.string(),
@@ -11,12 +13,14 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
+    const { fetchTransactions } = useContext(TransactionsContext)
+
     const { register, handleSubmit, formState: { isSubmitting } } = useForm<SearchFormInputs>({
         resolver: zodResolver(searchFormSchema),
     })
 
     async function handleSearchTransactions(data: SearchFormInputs) {
-        await new Promise(resolve => setTimeout(resolve, 2000))
+        await fetchTransactions(data.query)
     }
 
     return (
